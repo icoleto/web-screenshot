67c8e97a307a34dbad882f411e275269cae469b5
Iván Coleto <ivancoleto@gmail.com>
2020-08-11 10:28:39 +0200

    headless false & error handling on specific selector

--- services.js
+++ services.js
@@ -4,12 +4,17 @@ exports.analize = async (url) => {
   try {
     browser = await puppeteer.launch({
       args: ["--no-sandbox"],
-      //headless: false,
+      headless: false,
     });
     const page = await browser.newPage();
 
     await page.goto(url, { waitUntil: "networkidle0" });
-    await page.click(".more-photos");
+    await page.evaluate(() => {
+      let but = document.querySelector(".more-photos");
+      if (but) {
+        return Promise.resolve(but.click());
+      }
+    });
     await autoScroll(page);
     await page.setViewport({ width: 1920, height: 1080 });
 
