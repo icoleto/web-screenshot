8bc8599c3acd1766955d6454c4aa9ecedac6b6b0
Iván Coleto <ivancoleto@gmail.com>
2021-06-01 10:11:14 +0200

    validate prod environment & config heroku setting for reading commit hash

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
-const services = require('./services');
-const child_process = require('child_process');
-const LAST_COMMIT = child_process.execSync('git rev-parse --short HEAD')
+const services = require("./services");
+const VERSION = require("./package.json").version;
+
+if (process.env.NODE_ENV !== "production") {
+  const child_process = require("child_process");
+  const LAST_COMMIT = child_process
+    .execSync("git rev-parse HEAD")
+    .toString()
+    .trim();
+} else {
+  LAST_COMMIT = process.env.HEROKU_SLUG_COMMIT;
+}
 
 app.use(express.static("public"));
 
 app.get("/web", async (req, res) => {
-    console.log(req.query["url"]);
-    const {screenshot, hostname} = await services.analize(req.query["url"]);
-        var img = Buffer.from(screenshot, "base64");
+  console.log(req.query["url"]);
+  const { screenshot, hostname } = await services.analize(req.query["url"]);
+  var img = Buffer.from(screenshot, "base64");
 
-        res.writeHead(200, {
-          "Content-Type": "image/png",
-          "Content-Length": img.length,
-          "Content-Disposition": `attachment; filename="${hostname}-${new Date()
-            .toISOString()
-            .slice(0, 19)
-            .replace(/[:T]/g, "-")}.png"`,
-        });
-        res.end(img); 
+  res.writeHead(200, {
+    "Content-Type": "image/png",
+    "Content-Length": img.length,
+    "Content-Disposition": `attachment; filename="${hostname}-${new Date()
+      .toISOString()
+      .slice(0, 19)
+      .replace(/[:T]/g, "-")}.png"`,
+  });
+  res.end(img);
 });
 
 app.get("/", function (req, res) {
   res.sendFile("public/index.html");
-}); 
+});
 
 app.get("/version", function (req, res) {
-  res.send(LAST_COMMIT);
-}); 
+  res.send({ version: VERSION, commit: LAST_COMMIT });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at port ${port}`);
 });
-new Date().get
